4b664185240ceca749fb0c46fdd931d3cd3b4d36
ninedraft <>
2018-03-20 17:42:00 +0300

    formatter skeleton

--- delog.go
+++ delog.go
@@ -1,1 +1,19 @@
 package delog
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	_ logrus.Formatter = new(Formatter)
+)
+
+type Formatter struct {
+	formatter logrus.Formatter
+}
+
+func (dbgFormatter *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
+
+	msg, err := dbgFormatter.Format(entry)
+	return msg, err
+}
